6cfea3513cdc0816c3d4e28df5240df636542193
Thekote <dkglima@gmail.com>
2022-02-11 16:10:49 -0300

    desafio rails

--- app/models/phone.rb
+++ app/models/phone.rb
@@ -3,6 +3,11 @@
 class Phone < ApplicationRecord
   validates :number, presence: true, uniqueness: true
   validates :label, presence: true
-  validates :is_default, inclusion: { in: [true, false] }, uniqueness: { scope: :contact_id }, if: :is_default?
+  validates :is_default, inclusion: { in: [true, false] }
   belongs_to :contact
+  after_save :update_default_phone
+
+  def update_default_phone
+    Phone.where(contact:).where.not(id:).update_all(is_default: false)
+  end
 end
--- spec/models/phone_spec.rb
+++ spec/models/phone_spec.rb
@@ -14,4 +14,21 @@ describe Phone do
       it { should belong_to(:contact) }
     end
   end
+
+  describe 'phone main update callback' do
+    context 'when successful update default phone' do
+      let(:user){User.create name: 'user1', age: 10, bio: 'nice bio'}
+      let(:contact){Contact.create name: 'contact3', birthday: '10/05/1995', user_id: user.id}
+      let(:phone){Phone.create number: '1237896578', label: 'home', is_default: false, contact_id: contact.id}
+      let(:phone2){Phone.create number: '9999999999', label: 'home', is_default: true, contact_id: contact.id}
+
+      it 'should update all contact phones when a new phone is set as default' do
+        phone3 = Phone.new number: '999988888', label: 'home', is_default: true, contact_id: contact.id      
+        expect(phone.is_default).to be(false)
+        expect{phone3.save}.to change(phone2, :is_default)
+        expect(phone3.is_default).to be(true)
+      end
+    end  
+  end
+
 end
