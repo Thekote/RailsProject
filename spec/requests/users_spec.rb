c1ad40ad831e9e08aa20041041a10d3743929974
Thekote <dkglima@gmail.com>
2022-02-17 18:05:20 -0300

    test: add more tests to user routes

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -6,9 +6,22 @@ RSpec.describe 'Users', type: :request do
     end
 
     it 'assigns @users' do
-      user = User.create(name: "Jhon", age: 20, bio: "nice bio")
+      user = User.create!(name: 'Jhon', age: 20, bio: 'nice bio')
       get '/users' 
       expect(assigns(:users)).to eq([user]) 
     end  
+  end
+
+  describe 'GET /show' do
+    it 'returns http success' do
+      get '/users'
+      expect(response).to have_http_status(:success)
+    end
+    
+    it 'assigns @user' do
+      user = User.create!(name: 'Jhonny', age: 59, bio: 'I love axes')
+      get '/users'
+      expect(assigns(:users)).to eq([user])
+    end
   end
 end
