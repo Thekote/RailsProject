6e95fb9059b06743f0e0bc7dec90e8d1c526d92e
Thekote <dkglima@gmail.com>
2022-02-18 20:38:45 -0300

    refactor: changed users create action test to better reflect the context

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+require 'rails_helper'
+
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
     it 'returns http success' do
@@ -7,9 +10,9 @@ RSpec.describe 'Users', type: :request do
 
     it 'assigns @users' do
       user = User.create!(name: 'Jhon', age: 20, bio: 'nice bio')
-      get root_path 
-      expect(assigns(:users)).to eq([user]) 
-    end  
+      get root_path
+      expect(assigns(:users)).to eq([user])
+    end
   end
 
   describe 'GET /show' do
@@ -18,7 +21,7 @@ RSpec.describe 'Users', type: :request do
       get user_path(user.id)
       expect(response).to have_http_status(:success)
     end
-    
+
     it 'assigns @user' do
       user = User.create!(name: 'Jhonny', age: 59, bio: 'I love axes')
       get user_path(user.id)
@@ -34,9 +37,16 @@ RSpec.describe 'Users', type: :request do
   end
 
   describe 'POST /create' do
-    it 'returns http success' do
-      post '/users', :params => { :user => { name: 'dummy' } }
-      expect(response).to have_http_status(:redirect)
+    context 'when creating a user with valid params' do
+      it 'should create a new user' do
+        expect { post users_path({ user: { name: 'dummy' } }) }.to change { User.count }.by(1)
+      end
+    end
+
+    context 'when creating a user without valid params' do
+      it 'shloud not create a new user' do
+        expect { post users_path({ user: { name: '' } }) }.to_not change { User.count }
+      end
     end
   end
 
