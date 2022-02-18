ebdb48984406bc17f1509301eb2f43ff597b394f
Thekote <dkglima@gmail.com>
2022-02-18 15:27:34 -0300

    refactor: index routes on controller tests

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,13 +1,13 @@
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
     it 'returns http success' do
-      get '/users'
+      get root_path
       expect(response).to have_http_status(:success)
     end
 
     it 'assigns @users' do
       user = User.create!(name: 'Jhon', age: 20, bio: 'nice bio')
-      get '/users' 
+      get root_path 
       expect(assigns(:users)).to eq([user]) 
     end  
   end
